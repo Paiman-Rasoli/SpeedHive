4af97f278f0d77ce3f8c4accbba0512453a6aaee
Paiman-Rasoli <paimanrasoli789@gmail.com>
2025-12-13 00:56:16 +0430

    setup project

---
+++ src/routes/router.tsx
@@ -0,0 +1,11 @@
+import { createHashRouter } from "react-router-dom";
+import RootLayout from "../layouts/RootLayout";
+import HomePage from "../pages/HomePage";
+
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
+]);
